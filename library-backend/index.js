b2c4de6fb43728f25c46fbc2a0dbf28cc04668b6
Pia Manns <pia.manns@gmail.com>
2024-08-30 19:15:17 +0300

    Add exercise 8.5 (add optional genre parameter to allBooks query)

--- library-backend/index.js
+++ library-backend/index.js
@@ -98,7 +98,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -107,8 +107,14 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) return books
-      return books.filter((book) => book.author === args.author)
+      let filteredBooks = [...books]
+      if (args.author) {
+        filteredBooks = filteredBooks.filter((book) => book.author === args.author)
+      }
+      if (args.genre) {
+        filteredBooks = filteredBooks.filter((book) => book.genres.includes(args.genre))
+      }
+      return filteredBooks
     },
     allAuthors: () => authors,
   },
