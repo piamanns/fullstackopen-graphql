1c6f22cd41681bf99d96e3c0a3a382f7a231c288
Pia Manns <pia.manns@gmail.com>
2024-08-30 21:13:45 +0300

    Add exercise 8.6 (implement addBook mutation, also adding any new authors)

--- library-backend/index.js
+++ library-backend/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -101,6 +102,15 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String]
+    ): Book
+  }
 `
 const resolvers = {
   Query: {
@@ -121,6 +131,17 @@ const resolvers = {
   Author: {
     bookCount: (root) => books.reduce((count, book) =>
       book.author === root.name ? ++count : count, 0)
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      if (!(authors.find(a => a.name === args.author))) {
+        const author = { name: args.author, id: uuid() }
+        authors = authors.concat(author)
+      }
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+      return book
+    }
   }
 }
 
