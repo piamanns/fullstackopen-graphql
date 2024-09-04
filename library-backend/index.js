db98d1d91418909aac97ab75bc88d3c447dd53f6
Pia Manns <pia.manns@gmail.com>
2024-09-04 15:53:31 +0300

    Add exercise 8.7 (implement mutation for setting author birth year)

--- library-backend/index.js
+++ library-backend/index.js
@@ -110,6 +110,11 @@ const typeDefs = `
       published: Int!
       genres: [String]
     ): Book
+
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 const resolvers = {
@@ -141,6 +146,18 @@ const resolvers = {
       const book = { ...args, id: uuid() }
       books = books.concat(book)
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+      if (!author) {
+        return null
+      }
+      const updatedAuthor = {...author, born: args.setBornTo}
+      authors = authors.map(a => a.name === args.name
+        ? updatedAuthor
+        : a
+      )
+      return updatedAuthor
     }
   }
 }
