e25ce82444dabcf1093b766ee810a466a6ca8815
Pia Manns <pia.manns@gmail.com>
2024-09-09 20:08:36 +0300

    Add exercise 8.19 (books by genre part 1, filter using React)

--- library-frontend/src/components/Books.jsx
+++ library-frontend/src/components/Books.jsx
@@ -1,13 +1,34 @@
+import { useState } from 'react'
+
 const Books = (props) => {
+  const [filter, setFilter] = useState('')
+
   if (!props.show) {
     return null
   }
 
   const books = props.books
 
+  const genres = props.books.reduce((genreList, book) => {
+    if (book.genres) {
+      book.genres.forEach(genre => {
+        if (!genreList.includes(genre)) {
+          genreList.push(genre);
+        }
+      })
+    };
+    return genreList;
+  }, []);
+
   return (
     <div>
       <h2>books</h2>
+
+      {filter &&
+        <div>
+          in genre <strong>{filter}</strong>
+        </div>
+      }
 
       <table>
         <tbody>
@@ -16,15 +37,23 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {books
+            .filter((book) => filter ? book.genres.includes(filter) : book)
+            .map((book) => (
+              <tr key={book.title}>
+                <td>{book.title}</td>
+                <td>{book.author.name}</td>
+                <td>{book.published}</td>
+              </tr>
+            ))
+          }
         </tbody>
       </table>
+      <h3>Filter by genre: </h3>
+      {genres.map((genre) => (
+        <button key={genre} onClick={() => setFilter(genre)}>{genre}</button>
+      ))}
+      <button onClick={() => setFilter('')}>all genres</button>
     </div>
   )
 }
